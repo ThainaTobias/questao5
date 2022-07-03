7e66529c6c9bd419558e99e814c09af19cd7c084
ThainaTobias <alunathaina700@gmail.com>
2022-07-03 16:10:54 -0300

    criando menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,31 @@
-import Title from './Title';
-import Button from './Button';
-import Noticia from './Noticia';
-import "./App.css"
+import Noticia from './Noticia.jsx';
 
 function App() {
     const lista_noticias = [
         {
-            hour: "1",
-            description: "Funcionária que denunciou procurador, diz que agressor 'desprezava mulheres' e teme sair na rua:'Muito medo' "
+            description: "Tools",
         },        
         {
-            hour: "2",
-            description: "Imposto de Renda 2022: Receita abre consultas ao 2º lote de restituição nesta quinta; veja se você está no grupo"
+
+            description: "Pricing",
         },
         {
-            hour: "3",
-            description: "Governo opera para evitar CPI do MAC, que ganhou força após prisão de Milton Ribeiro"
+            description: "Blog",
         },
         {
-            hour:"4",
-            description: "Em Itajubá, assaltantes armados atacam agência da caixa econômica federal; 7 ficam feridos"
+    
+            description: "About",
         },
         {
-            hour:"5",
-            description:"Almir Sater é 'parado em blitz' e policiais tietam cantor:'O senhor é o dono dessa chalana?' ",
+            description:"Customers",
         },
     ];
 
     return (
         <div className='container'>
-            <Title />
 
             {lista_noticias.map((noticia) => {
-                return <Noticia horario={noticia.hour} desc={noticia.description} />
+                return <Noticia desc={noticia.description} />
             })}
 
         </div>
